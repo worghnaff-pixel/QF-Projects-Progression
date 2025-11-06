9d2623cba846ae9d52c6d47a98ba0e823b75d394
worghnaff-pixel <worghnaff@gmail.com>
2025-11-06 14:27:51 +0100

    update

--- Alt_Projects.py/Random_Strategy_vs_SP500.py
+++ Alt_Projects.py/Random_Strategy_vs_SP500.py
@@ -3,7 +3,7 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = yf.download('^GSPC',start = "2008-1-1",end="2009-1-1")
+data = yf.download('^GSPC',start = "2024-1-1",end="2025-1-1")
 
 def strat(data):
     rand = []
@@ -47,3 +47,39 @@ print(EV(prices))
 sp500value = (1+data['Close'].pct_change()).cumprod()
 print(sp500value.iloc[-1])
 plt.show()
+
+#implement adding money at random or adding money at drops or at highs
+#repeat with sp500
+"""
+def measureDD(data):
+    maxDD = []
+    maxoutliers = []
+    maxi = []
+    mini = []
+    minoutliers = []
+    parsed = []
+    for i in data['Close']:
+        parsed.append(data['Close'].iloc[data['Close'].iloc[i]])
+        if data['Close'].iloc[data['Close'].iloc[i]] > max(parsed):
+            maxoutliers.append(data['Close'].iloc[data['Close'].iloc[i]])
+            maxi.append(i)
+        if data['Close'].iloc[data['Close'].iloc[i]] < min(parsed):
+            minoutliers.append(data['Close'].iloc[data['Close'].iloc[i]])
+            mini.append(i)
+    for j in range(0,max(len(maxi) or len(mini))):
+        maxval = max(maxoutliers)
+        maxpos = maxoutliers.index(maxval)
+        maxcrit = maxi[maxpos]
+        minval = min(minoutliers)
+        minpos = minoutliers.index(minval) 
+        mincrit = mini[minpos]
+        if maxcrit > mincrit:
+            maxDD.append(1-mincrit/maxcrit)
+        else:
+            minoutliers.pop(mini)
+            mini.pop(minoutliers.index(minval))
+    return maxDD
+print(measureDD(data))
+
+#agghghghgh
+"""     
